d5e1b5883061d77a2f8416c6061cfaa61997675e
baumkuchen <koki@mba.local>
2023-04-22 23:56:14 +0900

    urql successs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider, createClient, cacheExchange, fetchExchange } from 'urql';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const client = createClient({
+  url: 'http://localhost:3010/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
+
+export default App;
