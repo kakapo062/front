76195b633dc58f1f3d14d8d2a0f1b63dbd925c73
baumkuchen <koki@mba.local>
2023-04-25 09:17:19 +0900

    top画面作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
 import { useBooks } from '../hooks/useBooks';
+import { useState } from 'react';
 
 const Home: React.FC = () => {
   const [result] = useBooks();
   const { data, fetching, error } = result;
 
-  if (fetching) return <p>Loading...</p>;
-  if (error) return <p>Oops! Something went wrong.</p>;
+  // if (fetching) return <p>Loading...</p>;
+  // if (error) return <p>Oops! Something went wrong.</p>;
 
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  }
+  const handleSearch = () => {
+    // 検索処理をここに書く
+  }
   return (
-    <div>
-      <h1>Books</h1>
-      <ul>
-        {data.books.map((book) => (
-          <li key={book.id}>{book.title}</li>
-        ))}
-      </ul>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Search for a book
+        </h1>
+        <div className="rounded-md shadow-sm">
+          <div>
+            <input
+              type="text"
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="本のタイトル"
+              value={searchTerm}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500"
+            onClick={handleSearch}
+          >
+            本を検索
+          </button>
+        </div>
+        {/* <ul>
+          {data?.books.map((book) => (
+            <li key={book.id}>
+              <p>{book.title}</p>
+            </li>
+          ))}
+        </ul> */}
+      </div>
     </div>
   );
 }
